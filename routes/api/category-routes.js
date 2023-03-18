b3170a22fc357dcd933d8622fb0648bace23a398
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-17 23:57:54 -0600

    added get route for to find a value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,16 +2,15 @@ const router = require("express").Router();
 const { Category, Product } = require("../../models");
 const log = console.log;
 
-// The `/api/categories` endpoint
+//get route to find all categories
 router.get("/", (req, res) => {
-  // find all categories
   Category.findAll({
     include: [
       {
         model: Product,
         attributes: ["id", "product_name", "price", "category_id"],
-      }
-    ]
+      },
+    ],
   })
     .then((data) => res.json(data))
     .catch((err) => {
@@ -19,15 +18,36 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+//get route to find one category by its 'id' value
 router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "category_id"],
+      },
+    ],
+  })
+    .then((data) => {
+      if (!data) {
+        res.json(404).json({ message: "There is no catergory with this id! " });
+        return;
+      }
+      res.json(data);
+    })
+    .catch((err) => {
+      log(err);
+      res.status(500).json(err);
+    });
 });
 
+//post route to create a new category
 router.post("/", (req, res) => {
-  // create a new category
 });
-
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
