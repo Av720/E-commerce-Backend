cec5fe0c4c5ffcd149ede644daaecd2bcdd3648d
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-19 00:18:22 -0600

    added delete route in order to delete one product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,8 +26,7 @@ router.get("/", (req, res) => {
 
 // get one product
 router.get("/:id", (req, res) => {
-
-  // this will obtain one product using the id 
+  // this will obtain one product using the id
   Product.findOne({
     where: {
       id: req.params.id,
@@ -58,15 +57,14 @@ router.get("/:id", (req, res) => {
 
 // create new product
 router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  //this route will create a new product
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tag_id,
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -131,7 +129,26 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-  // delete one product by its `id` value
+  // this route will delete one product by its `id` value
+
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      if (!data) {
+        res
+          .status(404)
+          .json({ message: "There is no product dound with that id!" });
+        return;
+      }
+      res.json(data);
+    })
+    .catch((err) => {
+      log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
