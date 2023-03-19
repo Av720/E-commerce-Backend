6c9dfcbc7f9b8e7be77c61c68c2f04ad6b2354dd
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-18 22:58:07 -0600

    defined and added all the references fro each column

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,43 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    //this will define id clumn
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // this will define the product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    // this will define the models id
+    tag_id: {
+      type: DataTypes.INTEGER,
+
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
