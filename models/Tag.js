0861bc4318c29f19f75d7cc5cb7db4b7859c071f
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-18 23:04:10 -0600

    defined the columns and references for the tag column

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // this will define the column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // this will define the tag column and set the type to string
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
